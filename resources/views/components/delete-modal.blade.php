c5034b8f772b1f9b35bddfae0902f90d39c450be
Mfrodriguezp <mildriz06@gmail.com>
2024-02-12 00:06:00 -0500

    Creación de componente modal para eliminación

---
+++ resources/views/components/delete-modal.blade.php
@@ -0,0 +1,13 @@
+<div class="px-6 py-4">
+    <div class="text-lg text-center font-medium text-gray-900">
+        {{$title}}
+    </div>
+
+    <div class="mt-4 text-sm text-gray-600">
+        {{$content}}
+    </div>
+
+</div>
+<div class="flex flex-row justify-end gap-2 px-6 py-4 bg-gray-100 text-end">
+    {{$footer}}
+</div>
