dcae87f4108a01d8431cb88b4a0c0f93994c849e
Mfrodriguezp <mildriz06@gmail.com>
2024-02-14 17:29:24 -0500

    Se actualizan los campos en el modelo de Evaluated Fragance

--- app/Livewire/Judments/JudmentsIndex.php
+++ app/Livewire/Judments/JudmentsIndex.php
@@ -2,11 +2,20 @@
 
 namespace App\Livewire\Judments;
 
+use App\Models\EvaluatedFragance;
 use Livewire\Component;
 
 class JudmentsIndex extends Component
 {
     public $title = "Pruebas de sustantividad";
+    public $evaluated;
+
+    public function mount(){
+        $this->evaluated=EvaluatedFragance::where('status_evaluation','En curso')
+        ->orderByDesc('id_evaluated_fragance','desc')
+        ->limit(1)
+        ->get();
+    }
 
     public function render()
     {
--- resources/views/livewire/judments/judments-index.blade.php
+++ resources/views/livewire/judments/judments-index.blade.php
@@ -1,4 +1,115 @@
 <div>
-    Hola desde el Judments
-    {{$title}}
+    <div class="header flex items-center mb-10">
+        <div class="title basis-3/5">
+            <span class="title">{{ $title }}</span>
+        </div>
+    </div>
+
+    <div class="data">
+        <div class="sutantive-test grid gap-4 grid-cols-4 my-6" id="carrier1">
+            {{-- Control 1 --}}
+            <div
+                class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Control inicial</h5>
+                </a>
+
+                <a href="#"
+                    class="btn-primary inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    Iniciar
+                </a>
+            </div>
+            {{-- Control 2 --}}
+            <div
+                class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Control 3 Hrs
+                    </h5>
+                </a>
+
+                <a href="#"
+                    class="btn-primary inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    Iniciar
+                </a>
+            </div>
+            {{-- Control 3 --}}
+            <div
+                class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Control 4 Hrs 30
+                    </h5>
+                </a>
+
+                <a href="#"
+                    class="btn-primary inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    Iniciar
+                </a>
+            </div>
+            {{-- Control 4 --}}
+            <div
+                class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Control 6 Horas
+                    </h5>
+                </a>
+                <a href="#"
+                    class="btn-primary inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    Iniciar
+                </a>
+            </div>
+        </div>
+        {{--Portador 2--}}
+        <div class="sutantive-test grid gap-4 grid-cols-4 my-6" id="carrier2">
+            {{-- Control 1 --}}
+            <div
+                class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Control inicial</h5>
+                </a>
+
+                <a href="#"
+                    class="btn-primary inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    Iniciar
+                </a>
+            </div>
+            {{-- Control 2 --}}
+            <div
+                class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Control 3 Hrs
+                    </h5>
+                </a>
+
+                <a href="#"
+                    class="btn-primary inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    Iniciar
+                </a>
+            </div>
+            {{-- Control 3 --}}
+            <div
+                class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Control 4 Hrs 30
+                    </h5>
+                </a>
+
+                <a href="#"
+                    class="btn-primary inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    Iniciar
+                </a>
+            </div>
+            {{-- Control 4 --}}
+            <div
+                class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Control 6 Horas
+                    </h5>
+                </a>
+                <a href="#"
+                    class="btn-primary inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    Iniciar
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
