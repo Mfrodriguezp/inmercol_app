edb77d4bf7faca881a0c4b25823fde3fdd3413ea
Mfrodriguezp <mildriz06@gmail.com>
2024-11-04 21:58:55 -0500

    fixed bug in create-edit-modal for show benchmark in database for evaluation edit

--- resources/views/livewire/admin/evaluateds/create-edit-evaluated-modal.blade.php
+++ resources/views/livewire/admin/evaluateds/create-edit-evaluated-modal.blade.php
@@ -130,7 +130,7 @@
                                     @endif
                                 </div>
                                 <div class="w-full p-2">
-                                    @if (isset($evaluatedFragance) && is_object($evaluatedFragance) && $benchmark == $fragance_test_code_1)
+                                    @if ((isset($evaluatedFragance) && is_object($evaluatedFragance)) && ($benchmark == $fragance_test_code_1))
                                         <input checked id="default-radio-1" type="radio"
                                             value="{{ $fragance_test_code_1 }}" name="benchmark"
                                             class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
@@ -187,7 +187,7 @@
                                     @endif
                                 </div>
                                 <div class="w-full p-2">
-                                    @if (isset($evaluatedFragance) && is_object($evaluatedFragance) && $benchmark == $fragance_test_code_1)
+                                    @if ((isset($evaluatedFragance) && is_object($evaluatedFragance)) && ($benchmark == $fragance_test_code_2))
                                         <input checked id="default-radio-2" type="radio"
                                             value="{{ $fragance_test_code_2 }}" name="benchmark"
                                             class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
